911c22f04f6c9b701b8a2dbe4edca6bec15f3abd
Cedric Joaquin <joaquin.cedric@gmail.com>
2020-05-08 01:06:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(int)
   int.positive? ? multiples = (2...int).to_a : multiples = (int+1..1).to_a
   multiples.delete(0)
   
-  int == 1 ? false : true
+  return false if int == 1
   
   !multiples.any? do |num|
     int % num == 0 
