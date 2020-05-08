1ac8b11fe5b8d443cda734c1dd40a43c35b81e88
Cedric Joaquin <joaquin.cedric@gmail.com>
2020-05-08 01:04:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 def prime?(int)
-  int.positive? ? multiples = (2...int).to_a : multiples = (int+1..-1).to_a
+  int.positive? ? multiples = (2...int).to_a : multiples = (int+1..1).to_a
   multiples.delete(0)
   
   !multiples.any? do |num|
